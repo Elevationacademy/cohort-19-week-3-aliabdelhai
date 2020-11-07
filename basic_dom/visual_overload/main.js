a3c96f9755ec7f0ec26bf8b7bec98cb92348edd9
aliabdelhai <72930982+aliabdelhai@users.noreply.github.com>
2020-11-07 13:45:08 +0200

    add Visual_OverLoad

---
+++ basic_dom/visual_overload/main.js
@@ -0,0 +1,30 @@
+
+
+const getRandomColor = function() {
+    const niceColors = ["#8e44ad", "#3498db", "#c0392b", "#f1c40f", "#d35400", "#2ecc71", "#1abc9c", "#2c3e50", "#7f8c8d"]
+    
+    const randomPosition = Math.floor(Math.random() * niceColors.length);
+    return niceColors[randomPosition];
+}
+
+
+for(let i=0; i<6; i++)
+{
+    let box = document.createElement("div")
+    box.setAttribute("class", "box")
+    box.style.backgroundColor = getRandomColor()
+    document.getElementById("container").appendChild(box)    
+
+}
+
+const randomBackGround = function(){
+    this.style.backgroundColor = getRandomColor()    
+}
+
+
+const boxes = document.getElementsByClassName("box")
+for(let b of boxes)
+{
+    b.onmouseover = randomBackGround
+}
+
